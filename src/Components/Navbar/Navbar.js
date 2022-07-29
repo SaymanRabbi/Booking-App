34bd7de0a9068db8ca66212c397d45d7c8edf04b
sayman <mdrabbi.asm@gmail.com>
2022-07-30 00:08:45 +0600

    navbar complete

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,47 +1,38 @@
 import React from 'react';
 import './Navbar.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons'
 const Navbar = () => {
     return (
-        <div class="navbar bg-base-100">
-        <div class="navbar-start">
+        <div class="navbar bg-base-100 px-5">
+            <div class="navbar-start">
+                
           <div class="dropdown">
             <label tabindex="0" class="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
+            {/* for mobile devices */}
             <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Item 1</a></li>
-              <li tabindex="0">
-                <a class="justify-between">
-                  Parent
-                  <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                </a>
-                <ul class="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
+              <li><a href='rent' className=' font-bold'>Rent</a></li>
+              <li><a href='Buy' className=' font-bold'>Buy</a></li>
+              <li><a href='Sell' className=' font-bold'>Sell</a></li>
+             
+              <li><a href='Resources' className=' font-bold'>Resources</a></li>
             </ul>
-          </div>
-          <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div class="navbar-center hidden lg:flex">
-          <ul class="menu menu-horizontal p-0">
-            <li><a>Item 1</a></li>
-            <li tabindex="0">
-              <a>
-                Parent
-                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-              </a>
-              <ul class="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
+                </div>
+                <a class="btn btn-ghost normal-case text-xl flex gap-1 text-primary" href='logo'> <FontAwesomeIcon icon={faEnvelopeOpenText}/> Estatery</a>
+                <div class=" hidden lg:flex navbar-start">
+            <ul class="menu menu-horizontal p-0">
+              {/* for desktop */}
+          <li><a href='rent' className=' font-bold'>Rent</a></li>
+              <li><a href='Buy' className=' font-bold'>Buy</a></li>
+              <li><a href='Sell' className=' font-bold'>Sell</a></li>
+             
+              <li><a href='Resources' className=' font-bold'>Resources</a></li>
           </ul>
         </div>
+        </div>
+       
         <div class="navbar-end flex gap-5">
         <button class="btn">Login</button>
         <button class="btn btn-primary">Sign Up</button>
